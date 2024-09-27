d62f7f844c72f883e60288ba787a36ff51cac822
touhid <touhid.islam96@gmail.com>
2024-09-27 10:44:43 +0200

    newly modified

---
+++ praindex.js
@@ -0,0 +1,32 @@
+// const numbers = [54, 87, 95, 89];
+
+// numbers[2] = 32;
+
+// console.log(numbers);
+// const apple = 20;
+// const orange = 13;
+// const kola = 27;
+// const coke = 15;
+
+// if (apple < orange) {
+//   console.log("ok");
+// } else if (kola < orange) {
+//   console.log("Esha");
+// } else {
+//   console.log("mojs loss");
+// }
+let piyajgiven = 3;
+
+while (piyajgiven < 6) {
+  console.log("are bhai parsen");
+  piyajgiven += 1;
+}
+
+const aArray = [25, 25, 95, 32, "apple"];
+
+for (let i = 0; i < aArray.length; i++) {
+  console.log(aArray[i]);
+  if ((aArray[i] = 95)) {
+    break;
+  }
+}
